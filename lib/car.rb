91ac93f36a58ba07e2a74f84bb35c6c93da4665f
Peter Camp <Pete.f.Camp@gmail.com>
2016-03-23 18:54:09 -0500

    ready to test

--- lib/car.rb
+++ lib/car.rb
@@ -2,31 +2,15 @@ require "pry"
 
 class Car
 
-	def initialize(options ={})
-		@type = options[:type]
-		@color = options[:color]
-		@mileage = options[:mileage]
+	attr_reader :type, :color, :mileage
+	attr_writer :type, :color, :mileage
+
+	def initialize(options={})
+		@type = options [:type]
+		@color = options [:color]
+		@mileage = options [:mileage] 
 	end	
 
-	def type
-		@type
-	end
-
-	def type=(new_type)
-		@type = new_type
-	end
-
-	def color
-		@color
-	end
-
-	def color=(new_color)
-		@color = new_color
-	end
-
-	def mileage
-		@mileage
-	end
 
 	def mileage=(new_mileage)
 		@mileage = new_mileage unless new_mileage < 1
--- lib/nba_player.rb
+++ lib/nba_player.rb
@@ -1,4 +1,10 @@
+require "pry"
+
+
 class NbaPlayer
+
+attr_reader :name, :position, :college, :birthday, :ppg
+attr_writer :name, :position, :college, :birthday
 
 	def initialize(options={})
 		@name = options[:name]
@@ -8,45 +14,13 @@ class NbaPlayer
 		@ppg = options[:ppg]
 	end
 
-	def name
-		@name
-	end
 
-	def name=(new_name)
-		@name = new_name
-	end
-
-	def position
-		@position
-	end
-
-	def postiion=(new_position)
-		@positon = new_position
-	end
-
-	def college
-		@college
-	end
-
-	def college=(new_college)
-		@college = new_college
-	end
-
-	def birthday
-		@birthday
-	end
-
-	def birthday=(new_birthday)
-		@birthday = new_birthday
-	end
-
-	def ppg
-		@ppg
-	end
 
 	def ppg=(new_ppg)
 		@ppg = new_ppg unless new_ppg < 1
 	end
 end
 
+
+binding.pry
 
